f387e80d50656fde44186dd1d9671b2945cea2b2
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-09-14 13:12:30 +1200

    Transferred yes_no from Lucky Unicorn project

---
+++ yes_no_v1.py
@@ -0,0 +1,18 @@
+# Ask the user if they have played before.
+show_instructions = input("Have you played this game before?").lower()
+while show_instructions.lower() != "xxx":
+
+    # If they say "yes", output "program continues".
+    # If they say no, output 'display instructions'.
+    # If the answer is invalid, print an error.
+
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
+        print("Program continues.")
+
+    elif show_instructions == "no" or "n":
+        show_instructions == "no"
+        print("Display instructions.")
+
+    else:
+        print("Please answer with either yes or no.")
