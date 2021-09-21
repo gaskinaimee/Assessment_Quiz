5df53f0554d7bbc801d91891038ecd45ca73ebec
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-09-21 14:58:49 +1200

    Added if/else statements.

---
+++ rounds_v1.py
@@ -0,0 +1,14 @@
+rounds = int(input("How many questions would you like to answer?"))
+questions_asked = 0
+
+if rounds == 5:
+    print("You have chosen to play 5 rounds!")
+elif rounds == 10:
+    print("You have chosen to play 10 rounds!")
+elif rounds == 15:
+    print("You have chosen to play 15 rounds!")
+elif rounds == 20:
+    print("You have chosen to play 20 rounds!")
+else:
+    ("Please enter either 5, 10, 15, or 20 rounds.")
+
