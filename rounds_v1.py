882451e74892c06b85a3872342c18e2848624bbb
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-09-27 11:40:58 +1300

    While loop added. Round increaser also added.

--- rounds_v1.py
+++ rounds_v1.py
@@ -1,4 +1,4 @@
-rounds = int(input("How many questions would you like to answer?"))
+rounds = int(input("How many rounds would you like to play?"))
 
 if rounds == 5:
     print("You have chosen to play 5 rounds!")
@@ -11,24 +11,14 @@ elif rounds == 20:
 else:
     ("Please enter either 5, 10, 15, or 20 rounds.")
 
-questions = ["What language is spoken in Brazil?",
-"How many bones are there in the average adult human body?",
-"Which country is brie cheese originally from?",
-"Which came first, the Jurassic or Triassic Period?",
-"Which planet has the most moons?",
-"Which country awards the Nobel Peace Prize?",
-"What does “www” stand for in a website browser?",
-"When was the first dictionary published?",
-"How many hearts does an octopus have?",
-"Who invented the word 'vomit'?",
-"What is the collective name for a group of camels?",
-"What is the name of the headteacher in Roald Dahl's Matilda?",
-"What is Japanese sake made from?",
-"What's the best selling book of all time?",
-"Who invented jeans?",
-"What do algophobics fear?",
-"Which creature in Greek mythology was half-man and half-bull?",
-"What chemical element has the atomic number 18?",
-"What is the fourth sign of the Zodiac?",
-"How many stripes are there on the US flag?"]
+play_again = input("Press <Enter> to play...").lower()
 
+
+while play_again == "":
+
+    # Increase number of rounds played.
+    rounds += 1
+
+
+    # Print round number
+    print("*** Round #{} ***".format(rounds))
