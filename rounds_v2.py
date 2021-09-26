9cf585970fb5602d45813bd4eb49fcd78e71d0ec
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-09-27 11:41:19 +1300

    While loop added. Round increaser also added.

---
+++ rounds_v2.py
@@ -0,0 +1,26 @@
+rounds_played = 0
+rounds = int(input("How many rounds would you like to play?"))
+
+if rounds == 5:
+    print("You have chosen to play 5 rounds!")
+elif rounds == 10:
+    print("You have chosen to play 10 rounds!")
+elif rounds == 15:
+    print("You have chosen to play 15 rounds!")
+elif rounds == 20:
+    print("You have chosen to play 20 rounds!")
+else:
+    ("Please enter either 5, 10, 15, or 20 rounds.")
+
+play_again = input("Press <Enter> to play...").lower()
+
+
+while play_again == "":
+
+    # Increase number of rounds played.
+    rounds_played += 1
+
+
+    # Print round number
+    print("*** Round #{} ***".format(rounds_played))
+
