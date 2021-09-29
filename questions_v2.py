22cbe35a69ff023f273e28a4183ab32a18836cf5
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-09-29 14:43:04 +1300

    Questions randomly shuffle.

--- questions_v2.py
+++ questions_v2.py
@@ -1,3 +1,5 @@
+import random
+
 questions = ["What language is spoken in Brazil?",
              "How many bones are there in the average adult human body?",
              "Which country is brie cheese originally from?",
@@ -12,7 +14,8 @@ questions = ["What language is spoken in Brazil?",
              "What is the name of the headteacher in Roald Dahl's Matilda?",
              "What is Japanese sake made from?",
              "What's the best selling book of all time?",
-             "Who invented jeans?", "What do algophobics fear?",
+             "Who invented jeans?",
+             "What do algophobics fear?",
              "Which creature in Greek mythology was half-man and half-bull?",
              "What chemical element has the atomic number 18?",
              "What is the fourth sign of the Zodiac?",
@@ -21,3 +24,6 @@ questions = ["What language is spoken in Brazil?",
 answers = ["Portuguese", 206, "France", "The Triassic Period", "Saturn", "Norway", "World Wide Web", 1604, 3,
            "William Shakespeare", "A caravan", "Miss Trunchbull", "Rice", "The bible", "Levi Strauss", "Pain",
            "Minotaur", "Argon", "Cancer", 13]
+
+random.shuffle(questions)
+print(questions[0])
