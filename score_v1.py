389ab5c205923a8ed64c70659294b13b07276b80
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-11-10 14:53:30 +1300

    Finished score

---
+++ score_v1.py
@@ -0,0 +1,11 @@
+score = 0
+
+if score <= 5:
+    print("You got a lot wrong. You have a bit to work on!")
+elif score <= 10:
+    print("Nice try! You still have a little bit to work on!")
+elif score > 10:
+    print("Nice work! You got more than half right! I think next time you can get a higher score!")
+elif score == 20:
+    print("Woah, you did great! Awesome!")
+
