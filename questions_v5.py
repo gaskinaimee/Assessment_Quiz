419a699684cf275d2a954630a7f0a089b70035d4
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2021-11-10 14:53:10 +1300

    Treid different layout

---
+++ questions_v5.py
@@ -0,0 +1,168 @@
+question_list = ["What language is spoken in Brazil?",
+                 "How many bones are there in the average adult human body?",
+                 "Which country is brie cheese originally from?",
+                 "Which came first, the Jurassic or Triassic Period?",
+                 "Which planet has the most moons?",
+                 "Which country awards the Nobel Peace Prize?",
+                 "What does “www” stand for in a website browser?",
+                 "When was the first dictionary published?",
+                 "How many hearts does an octopus have?",
+                 "Who invented the word 'vomit'?",
+                 "What is the collective name for a group of camels?",
+                 "What is the name of the headteacher in Roald Dahl's Matilda?",
+                 "What is Japanese sake made from?",
+                 "What's the best selling book of all time?",
+                 "Who invented jeans?", "What do algophobics fear?",
+                 "Which creature in Greek mythology was half-man and half-bull?",
+                 "What chemical element has the atomic number 18?",
+                 "What is the fourth sign of the Zodiac?",
+                 "How many stripes are there on the US flag?"
+                 "What's the biggest animal in the world?"]
+
+answer_list = ["portuguese",
+               "206",
+               "france",
+               "triassic period",
+               "saturn",
+               "norway",
+               "world wide web",
+               "1604",
+               "3",
+               "william shakespeare",
+               "caravan",
+               "miss trunchbull",
+               "rice",
+               "the bible",
+               "levi strauss",
+               "pain",
+               "minotaur",
+               "argon",
+               "cancer",
+               "13"
+               "blue whale"]
+
+
+
+question1 = input(question_list[0]).lower()
+if question1 == "portugese":
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question2 = input(question_list[1]).lower()
+if question2 == answer_list[1]:
+
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question3 = input(question_list[2]).lower()
+if question3 == answer_list[2]:
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question4 = input(question_list[3]).lower()
+if question4 == answer_list[3]:
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question5 = input(question_list[4]).lower()
+if question5 == answer_list[4]:
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question6 = input(question_list[5]).lower()
+if question6 == answer_list[5]:
+    print("Correct!")
+else:
+    print("Wrong!")
+
+question7 = input(question_list[6]).lower()
+if question7 == answer_list[6]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[6]))
+
+question8 = input(question_list[7]).lower()
+if question8 == answer_list[7]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[7]))
+
+print("ROUND {}".format(rounds))
+question9 = input(question_list[8]).lower()
+if question9 == answer_list[8]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[8]))
+
+question10 = input(question_list[9]).lower()
+if question10 == answer_list[9]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[9]))
+
+
+question11 = input(question_list[10]).lower()
+if question11 == answer_list[10]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[10]))
+
+question12 = input(question_list[11]).lower()
+if question12 == answer_list[11]:
+    score += 1
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[11]))
+
+question13 = input(question_list[12]).lower()
+if question13 == answer_list[12]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[12]))
+
+question14 = input(question_list[13]).lower()
+if question14 == answer_list[13]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[13]))
+print("ROUND {}".format(rounds))
+question15 = input(question_list[14]).lower()
+if question15 == answer_list[14]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[14]))
+
+question16 = input(question_list[15]).lower()
+if question16 == answer_list[15]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[15]))
+
+question17 = input(question_list[16]).lower()
+if question17 == answer_list[16]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[16]))
+
+question18 = input(question_list[17]).lower()
+if question18 == answer_list[17]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[17]))
+
+question19 = input(question_list[18]).lower()
+if question19 == answer_list[18]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[18]))
+
+question20 = input(question_list[19]).lower()
+if question20 == answer_list[19]:
+    print("Correct! Your score is {}.".format(score))
+else:
+    print("Wrong! The correct answer is {}.".format(answer_list[19]))
